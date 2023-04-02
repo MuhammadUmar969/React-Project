b89020e7a1d2ca43049abf024ea2f45b53cef880
MuhammadUmar969 <muhammadumarazizkhan23@gmail.com>
2023-04-02 20:16:51 +0500

    Resturant Website

---
+++ src/component/Res/ApiRes.jsx
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "../image/maggi.jpg",
+      name: "Maggi",
+      category: "breakfast",
+      price: "75 rs",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "../image/allupakoida.jpg",
+      name: "Allu pakoida",
+      category: "evening",
+      price: "40 rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "../image/corn.jpg",
+      name: "Corn",
+      category: "breakfast",
+      price: "80 rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../image/chola.jpg",
+      name: "Chola",
+      category: "lunch",
+      price: "100 rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../image/pizza.jpg",
+      name: "Pizza",
+      category: "evening",
+      price: "500 rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../image/nonvegthali.jpg",
+      name: "Thali",
+      category: "dinner",
+      price: "180 rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../image/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "300 rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../image/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "400 rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../image/samosa.jpg",
+      name: "Samaso",
+      category: "evening",
+      price: "30 rs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
